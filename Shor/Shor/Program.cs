977b2c38f4877d270bb1698de213ab08844012bf
Nikita94_ <niksir20@mail.ru>
2015-12-06 16:20:08 +0300

    Add Rosenbrock's function

--- Shor/Shor/Program.cs
+++ Shor/Shor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Shor
 {
@@ -17,6 +18,17 @@ namespace Shor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            List<double> x = new List<double>();
+            int N = 3;
+            Random ran = new Random();
+            for (int i = 0; i < N; i++)
+                x[i] = ran.Next(-3, 3);
+
+            Rosenbrock ros = new Rosenbrock(N, x);
+            Console.WriteLine( ros.getSum());
+
+
         }
     }
 }
---
+++ Shor/Shor/Rosenbrock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Shor
+{
+    class Rosenbrock 
+    {
+        private int N;
+        private List<double> x;
+
+        
+
+        public Rosenbrock(int N, List<double> x) {
+            this.N = N;
+            this.x = x;
+        }
+
+        public double getSum() {
+            double f = 0.0;
+            Random ran = new Random();
+            for (int i = 0; i < x.Count - 1; i++)
+            {
+                f += Math.Pow(1 - x[i], 2) + 100 * ran.Next(0, 1) * Math.Pow(x[i + 1] - x[i] * x[i], 2);
+            }
+            return f;
+        }
+
+
+    }
+}
